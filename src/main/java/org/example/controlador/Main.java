8c7fcce7cab6f24fa13f3c5bdce5e35b04bf6acb
Yahir Rodríguez <126041734+yajirxz@users.noreply.github.com>
2023-05-26 19:22:16 -0600

    Add files via upload

---
+++ src/main/java/org/example/controlador/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.controlador.ControladorLibro;
+import org.example.vista.VentanaLibro;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaLibro view = new VentanaLibro("MVC Y JDBC");
+        ControladorLibro controller = new ControladorLibro(view);
+
+    }
+
+}
